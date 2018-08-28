ffa1d954ef2864cf8544c074005a0938602f6634
zhongqifan <zhongqifan@021.com>
2018-08-28 19:59:10 +0800

    命名

--- app_lib_interface/src/main/java/com/billy/app_lib_interface/H5RegisterManager.java
+++ app_lib_interface/src/main/java/com/billy/app_lib_interface/H5RegisterManager.java
@@ -8,15 +8,15 @@ import java.util.Set;
  * @since 17/9/20 16:56
  */
 public class H5RegisterManager {
-    private static HashMap<String, IH5Delegate> CATEGORIES = new HashMap<>();
+    private static HashMap<String, IH5Delegate> pageMap = new HashMap<>();
 
     static void register(IH5Delegate category) {
         if (category != null) {
-            CATEGORIES.put(category.getFilter(), category);
+            pageMap.put(category.getFilter(), category);
         }
     }
 
     public static Set<String> getCategoryNames() {
-        return CATEGORIES.keySet();
+        return pageMap.keySet();
     }
 }
